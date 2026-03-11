c4b65526190beeb369e7b866aec7d8076200c564
somanshuvyas17 <sv7375@srmist.edu.in>
2026-03-11 13:05:27 +0530

    UC2 HelloApp

--- helloapp.java
+++ helloapp.java
@@ -25,8 +25,7 @@
 public class helloapp {
 
     public static void main(String[] args) {
-        String name = args[0];
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, World!");
     }
 
 }
